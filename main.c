8983a841661a8617f2caa33db6ebee9466eb13f6
jackhasakeyboard <jackhasakeyboard@gmail.com>
2024-08-12 06:26:16 -0600

    Created serialized string for a cronjob that gets run every day

--- main.c
+++ main.c
@@ -1,3 +1,25 @@
+#include <stdio.h>
+
+const char glob = '*';
+
+void err(char* msg) {
+	fprintf(stderr, "Error: %s\n", msg);
+}
+
 int main(int argc, char* argv[]) {
+	if (argc == 1) {
+		err("No arguments");
+
+		return 1;
+	}
+
+	if (argc != 1 + 1) {
+		err("Wrong number of arguments");
+
+		return 1;
+	}
+
+	printf("0 0 %c %c %c %s", glob, glob, glob, argv[1]);
+
 	return 0;
 }
